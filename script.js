a6ca73383f1cf8fcba1a46bb7bc42bb99d022665
Caleb <fishfilli@outlook.com>
2022-11-19 18:38:41 -0800

    Add resetCellColor()

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ function changeColor(e) {
     e.target.style.backgroundColor = 'black';
 }
 
+function resetCellColor() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    for (let i = 0; i < gridItems.length; i++) {
+        const cell = gridItems[i];
+        cell.style.backgroundColor = 'white';
+    }
+}
 
 const changeDimensionBtn = document.querySelector('#change-squares-btn');
 changeDimensionBtn.addEventListener('click', changeDimension);
@@ -22,8 +29,11 @@ changeDimensionBtn.addEventListener('click', changeDimension);
 function changeDimension() {
     let userInput = parseInt(prompt('Please enter a whole number between 2 and 100.', '2'));
     if (Number.isInteger(userInput) && (userInput >= 2) && (userInput <= 100)) {
+        resetCellColor();
         createGrid(userInput, userInput);
     } else {
         alert('Please enter a valid number!');
     }
 }
+
+createGrid(16, 16); // initial dimensions upon page load
